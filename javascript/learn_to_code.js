b13070bcebb43313282f55a08c45594c956bfaf8
Martin Nnachi <mnnachi@gmail.com>
2019-05-08 14:21:54 +0100

    First commit

---
+++ javascript/learn_to_code.js
@@ -0,0 +1,20 @@
+// var name = "Martin";
+// var age = 23;
+// var shootingScore = 45.6;
+//
+// var message = "Hi, my name is " + name + " and i am " + age + " years old.";
+
+// alert(message);
+
+// var firstName = "Martin";
+// var lastName = "Nnachi";
+// var dob = "06-09-79";
+// var age = 39;
+// var profileImgUrl = "http://coolpicks.com/johnjacob.jpg";
+//
+// var loginWelcomeMessage = "Welcome, " + firstName + " " + lastName + ". Happy " + age + "th" + " birthday!";
+//
+// alert(loginWelcomeMessage)
+// console.log(loginWelcomeMessage)
+
+
